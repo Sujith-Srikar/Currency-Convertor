47f69bc349b70473d75b8c8045ada310af294718
Sujith Srikar <kandregulasujith@gmail.com>
2024-05-08 21:52:49 +0530

    api modified

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -4,9 +4,10 @@ function useCurrencyInfo(currency){
     const [data,setData] = useState({});
     useEffect(()=>{
         fetch(
-          `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-        ).then((res)=>res.json())
-         .then((res)=>setData(res[currency]))
+          `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        )
+          .then((res) => res.json())
+          .then((res) => setData(res[currency]));
     },[currency])
     return data;
 }
